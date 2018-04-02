09943bd1f2b6513257561dbb7245cb3591802f50
Kevin Haggerty <kghaggerty@gmail.com>
2018-04-02 11:10:29 -0500

    capturing input value of store name

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
-    handClick() {
-        alert('HEYYYY!')
+    myInput = React.createRef();
+    goToStore = (event) => {
+        //stop form from submitting
+        event.preventDefault();
+        //get text from that input
+        console.log(this.myInput);
+        //change the page/store whatever they entered
     }
-    goToStore() {
-        console.log("go to store")
-    }
+
     render() {
         return (
         <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please Enter A Store</h2>
-            <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
+            <input type="text" required placeholder="Store Name" ref={this.myInput} defaultValue={ getFunName() } />
             <button type="submit">Visit Store</button>
         </form>
         )
