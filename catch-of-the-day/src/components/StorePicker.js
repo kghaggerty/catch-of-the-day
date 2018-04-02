404065eb04ca27efb6c9e8df1a14e04fd7b9f61d
Kevin Haggerty <kghaggerty@gmail.com>
2018-04-02 10:33:46 -0500

    added routes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
+    handClick() {
+        alert('HEYYYY!')
+    }
+    goToStore() {
+        console.log("go to store")
+    }
     render() {
         return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please Enter A Store</h2>
-            <input type="text" required placeholder="Store Name" />
+            <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
             <button type="submit">Visit Store</button>
         </form>
         )
